28c41b17ab8b4ccc070a5396e389bbfbf904dff0
uuutako <okn.sk009@gmail.com>
2021-01-27 16:55:46 +0900

    commit

---
+++ Hai3.java
@@ -0,0 +1,11 @@
+public class Hai3 {
+  public static void main(String[] args){
+    int arr[] = {2,4,6,8,10};
+    int sum = 0;
+
+    for( int i = 0; i <=4; i++ ) {
+      sum += arr[i];
+      }
+      System.out.println(sum);
+  }
+}
---
+++ Nest.java
@@ -0,0 +1,11 @@
+public class Nest {
+  public static void main(String[] args) {
+  
+    for(int i = 0; i <= 2; i++) {
+      for(int j = 0; j <= 2; j++) {
+        System.out.println( i + "-" + j );
+      }
+    }
+  }
+  
+}

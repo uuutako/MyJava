f8887ab91549db28453fbe157fce11271b813a0d
uuutako <okn.sk009@gmail.com>
2021-01-27 16:23:47 +0900

    commit

---
+++ Huku.java
@@ -0,0 +1,16 @@
+public class Huku {
+  public static void main(String[] args){
+    int x = 8;
+    int y = 12;
+    int z = 20;
+    x++;
+    y--;
+
+    System.out.println(x += 10);
+    System.out.println(z += y);
+
+    System.out.println(x);
+    System.out.println(y);
+  }
+  
+}

723e6a8291792c66728ba221fd70e72932770195
uuutako <okn.sk009@gmail.com>
2021-01-27 16:32:52 +0900

    commit

---
+++ If.java
@@ -0,0 +1,15 @@
+public class If {
+  public static void main(String[]args){
+    int age = 0;
+
+    if (age >=20){
+        System.out.println("adult");
+    } else if(age==0) {
+        System.out.println("baby");
+    } else {
+        System.out.println("child");
+    }
+  }
+
+  
+}

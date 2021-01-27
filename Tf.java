8eda327dc09a936f63f1c7d8fac9074b76577d59
uuutako <okn.sk009@gmail.com>
2021-01-27 16:12:29 +0900

    commit

---
+++ Tf.java
@@ -0,0 +1,13 @@
+public class Tf {
+  public static void main(String[]args){
+    int x = 10;
+    int y = 2;
+    int z = 10;
+
+    System.out.println(x>=y);
+    System.out.println(x<=y);
+    System.out.println(x<=z);
+
+  }
+
+}
---
+++ toka.java
@@ -0,0 +1,9 @@
+public class toka {
+  public static void main(String[]args){
+    int x = 10;
+    int y = 2;
+
+    System.out.println(x==y);
+    System.out.println(x != y);
+  }
+}
